efb82a960f2b527c98b0395701d1244296d68348
valsoh <vjxsoh001@mymail.sim.edu.sg>
2021-10-04 15:59:34 +0800

    adding spyder file

---
+++ spyder/pythonscript.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+integers = ["1" , "2" , "3", "4" , "5" , "6" , "7" , "8" , "9" , "10"]
+for k in integers:
+    print(k)
