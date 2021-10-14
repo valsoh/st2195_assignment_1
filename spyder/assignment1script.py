f1133f285629a187795cc017ac121f94525bd758
valsoh <vjxsoh001@mymail.sim.edu.sg>
2021-10-14 15:32:56 +0800

    pythonscriptfile added

---
+++ spyder/assignment1script.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 15:11:57 2021
+
+@author: valer
+"""
+
+integers = ["1" , "2" , "3", "4" , "5" , "6" , "7" , "8" , "9" , "10"]
+for k in integers:
+    print(k)
